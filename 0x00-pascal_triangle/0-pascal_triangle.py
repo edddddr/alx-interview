07a86b4da9c2d7e4d7225ab52ce89404d10e39d1
edddddr <endt6342@gmail.com>
2023-09-28 00:20:48 -0700

    pasc tr

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,23 @@
+#!/usr/bin/python3
+"""
+That conssit pascal triangle
+"""
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+    """
+    Print the triangle
+    """
+    triangle = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle[i-2][j-1] + triangle[i-2][j]
+                row.append(n)
         triangle.append(row)
+
     return triangle
